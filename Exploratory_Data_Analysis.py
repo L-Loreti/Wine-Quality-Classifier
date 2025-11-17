69165e2ea9b58259ee9cdf1b48215a5d40d84329
Leonardo Loreti <leonardoloreti14@gmail.com>
2025-11-17 16:13:50 -0300

    Ajuntamento de classes, transformação de features, cálculo de VIF, matriz de correlação de Pearson e Scatterplots

--- Exploratory_Data_Analysis.py
+++ Exploratory_Data_Analysis.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from statsmodels.tools.tools import add_constant
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
-wine = pd.read_csv('/content/gdrive/MyDrive/Ciência de Dados/Qualidade do Vinho - Classificação/WineQT.csv')
+wine = pd.read_csv('WineQT.csv')
 # Same data than the dataframe index
 wine = wine.drop(['Id'], axis = 1)
 
@@ -20,38 +20,87 @@ print(wine.info())
 print(wine.duplicated().sum())
 '''
 
-# It is always important to check if there are data patterns that can
-# result in a low value for the Pearson Correlation coefficient, even
-# if there is a strong relation between the variables. 
+# Let's look at the quantity of data for each class
 '''
-pd.plotting.scatter_matrix(wine, figsize = (22,22))
-'''
-
-# It is possible to realize some outliers by looking at the features
-# scatterplots with the 'quality' index
-
-wine_outliers = wine.loc[wine['total sulfur dioxide'] > 250].copy()
-wine_normal = wine.loc[wine['total sulfur dioxide'] < 250].copy()
-
-df_columns = wine.drop(['quality'], axis = 1).copy().columns.tolist()
-
-n_rows = 4
-n_cols = 3
-
-fig, axs = plt.subplots(n_rows, n_cols, figsize = (12, 12))
-
-for i in range(n_rows):
-  for j in range(n_cols):
-    if i*n_cols + j != n_rows*n_cols - 1:
-      axs[i, j].scatter(wine_normal['quality'], wine_normal[df_columns[i*n_cols + j]], s = 10)
-      axs[i, j].scatter(wine_outliers['quality'], wine_outliers[df_columns[i*n_cols + j]], 
-                        color = 'red', s = 10)
-      axs[i, j].set_xlabel('Quality')
-      axs[i, j].set_ylabel(df_columns[i*n_cols + j])
-
-plt.tight_layout()
+plt.hist(wine['quality'])
+plt.xlabel('Quality')
+plt.ylabel('Quantity')
 plt.show()
+'''
+# There is a high inbalance between the classes quantity of data, so 
+# let's join some classes
 
+wine['quality'].loc[wine['quality'] == 3] = 1
+wine['quality'].loc[wine['quality'] == 4] = 1
+wine['quality'].loc[wine['quality'] == 5] = 1
+wine['quality'].loc[wine['quality'] == 6] = 2
+wine['quality'].loc[wine['quality'] == 7] = 3
+wine['quality'].loc[wine['quality'] == 8] = 3
+
+# Plot histogram after classes change
+'''
+plt.hist(wine['quality'])
+plt.xlabel('Quality')
+plt.ylabel('Quantity')
+plt.show()
+'''
+###############################################
+# VIF ANALYSIS BEFORE FEATURES TRANSFORMATION #
+###############################################
+'''
+# Adding constant for the VIF regression procedure
+x = add_constant(wine)
+
+VIF = pd.Series([variance_inflation_factor(x.values, i) for i in range(x.shape[1])], index=x.columns)
+print(VIF)
+'''
+#############################################################
+# PEARSON CORRELATION MATRIX BEFORE FEATURES TRANSFORMATION #
+#############################################################
+'''
+# Pearson correlation coefficient matrix
+corr = wine.corr()
+
+# Generate a mask for the upper triangle
+mask1 = np.triu(np.ones_like(corr, dtype=bool))
+
+threshold = 0.3
+mask2 = abs(corr) < threshold
+
+# Draw the heatmap with the mask and correct aspect ratio
+
+sns.heatmap(corr, mask = mask1 | mask2, cmap='inferno', vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
+'''
+############################
+# VARIABLES TRANSFORMATION #
+############################
+wine['total acidity'] = wine['fixed acidity'] + wine['volatile acidity']
+wine['citric acid percentage'] = wine['citric acid']/wine['total acidity']
+wine['free sulfur dioxide percentage'] = wine['free sulfur dioxide']/wine['total sulfur dioxide']
+wine['percentage of alcohol density'] = (wine['alcohol']/100)/wine['density']
+
+quality_col = wine.pop('quality')
+wine.insert(len(wine.columns), 'quality', quality_col)
+
+wine = wine.drop(['fixed acidity', 'volatile acidity', 'citric acid',
+                  'free sulfur dioxide', 'total sulfur dioxide',
+                  'alcohol', 'density', 'pH'], axis = 1)
+
+##############################################
+# VIF ANALYSIS AFTER FEATURES TRANSFORMATION #
+##############################################
+'''
+# Adding constant for the VIF regression procedure
+x = add_constant(wine)
+
+VIF = pd.Series([variance_inflation_factor(x.values, i) for i in range(x.shape[1])], index=x.columns)
+print(VIF)
+'''
+############################################################
+# PEARSON CORRELATION MATRIX AFTER FEATURES TRANSFORMATION #
+############################################################
 
 # Pearson correlation coefficient matrix
 corr = wine.corr()
@@ -62,63 +111,43 @@ mask1 = np.triu(np.ones_like(corr, dtype=bool))
 threshold = 0.4
 mask2 = abs(corr) < threshold
 
-# Set up the matplotlib figure
-# f, ax = plt.subplots(figsize=(11, 9))
-
-# Generate a custom diverging colormap
-# cmap = sns.diverging_palette(230, 20, as_cmap=True)
-
 # Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(corr, mask = mask1 | mask2, cmap='inferno', vmax=.3, center=0,
+
+sns.heatmap(corr, mask = mask1 | mask2, cmap='inferno', vmin = -1, vmax=1, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
 
-# Adding constant for the VIF regression procedure
+#############################################
+# SCATTER PLOTS FOR ALL FEATURES AND TARGET #
+#############################################
+# It is always important to check if there are data patterns that can
+# result in a low value for the Pearson Correlation coefficient, even
+# if there is a strong relation between the variables. 
 '''
-x = add_constant(wine.drop(['Id'], axis = 1))
-
-VIF = pd.Series([variance_inflation_factor(x.values, i) for i in range(x.shape[1])], index=x.columns)
-print(VIF)
+pd.plotting.scatter_matrix(wine, figsize = (22,22))
+plt.show()
 '''
-
-#############################
-# EXPLORATORY DATA ANALYSIS #
-#############################
+########################################################
+# SCATTER PLOT FOR TARGET WITH THE OTHER FEATURES ONLY #
+########################################################
 '''
-wine = pd.read_csv('/content/gdrive/MyDrive/Ciência de Dados/Qualidade do Vinho - Classificação/WineQT.csv')
+df_columns = wine.drop(['quality'], axis = 1).copy().columns.tolist()
 
-wine_correct = wine.loc[wine['total sulfur dioxide'] < 250]
+n_rows = 2
+n_cols = 4
 
-wine_correct['total acidity'] = wine_correct['fixed acidity'] + wine_correct['volatile acidity']
-wine_correct['citric acid percentage'] = wine_correct['citric acid']/wine_correct['total acidity']
-wine_correct['free sulfur dioxide percentage'] = wine_correct['free sulfur dioxide']/wine_correct['total sulfur dioxide']
-wine_correct['percentage of alcohol density'] = (wine_correct['alcohol']/100)/wine_correct['density']
+fig, axs = plt.subplots(n_rows, n_cols, figsize = (12, 12))
 
-wine_correct = wine_correct.drop(['Id','fixed acidity', 'volatile acidity', 'citric acid',
-                                'free sulfur dioxide', 'total sulfur dioxide',
-                                'alcohol', 'density', 'pH'], axis = 1)
+for i in range(n_rows):
+  for j in range(n_cols):
+    if i*n_cols + j != n_rows*n_cols - 1:
+      axs[i, j].scatter(wine['quality'], wine[df_columns[i*n_cols + j]], s = 10)
+      # axs[i, j].scatter(wine_normal['quality'], wine_normal[df_columns[i*n_cols + j]], s = 10)
+      # axs[i, j].scatter(wine_outliers['quality'], wine_outliers[df_columns[i*n_cols + j]], 
+      #                   color = 'red', s = 10)
+      axs[i, j].set_xlabel('Quality')
+      axs[i, j].set_ylabel(df_columns[i*n_cols + j])
 
-# Calculation of VIF
-x = wine_correct.drop(['quality'], axis = 1)
-y = wine_correct['quality']
-
-# Adding constant for the VIF regression procedure
-x = add_constant(x)
-
-VIF = pd.Series([variance_inflation_factor(x.values, i) for i in range(x.shape[1])], index=x.columns)
-print(VIF)
-
-# x = wine_correct.drop(['quality'], axis = 1)
-# y = wine_correct['quality']
-
-# y.loc[y == 3] = 1
-# y.loc[y == 4] = 1
-# y.loc[y == 5] = 2
-# y.loc[y == 6] = 2
-# y.loc[y == 7] = 3
-# y.loc[y == 8] = 3
-'''
-# Plot histogram of quantity of data per quality
-'''
-plt.hist(y)
+plt.tight_layout()
 plt.show()
 '''
--- README.txt
+++ README.txt
@@ -1,3 +1,18 @@
 [15/11/2025] Primeio commit do projeto.
 
 [16/11/2025] Início da análise exploratória.
+[17/11/2025] 	A quantidade de dados para as diferentes classes
+		difere bastante, então é possível juntar algumas
+		classes de modo a aumentar a quantidade de dados.
+
+		Há algumas features correlacionadas (coef > 0.3),
+		então podemos tentar transformar tais variáveis 
+		de modo a descorrelacioná-las, e diminuir o VIF.
+		
+		Após a transformação das variáveis, todos os VIFs
+		ficaram próximos a um, i.e., a multicolinearidade
+		foi suprimida. Porém, mesmo antes da transformação
+		de variáveis, nenhuma feature apresentava grande
+		correlação com o target, somente a porcentagem de
+		densidade de álcool tem uma correlação maior, o 
+		que pode dificultar o aprendizado dos algoritmos.
